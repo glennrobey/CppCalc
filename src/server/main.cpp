d5cf63dd87de572f7aadc0c4a7400509670359c1
Glenn Robey <glennrobey95@gmail.com>
2026-07-14 14:56:25 -0500

    feat: add API error handling!

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -11,16 +11,41 @@ int main() {
           [&calc](
               const drogon::HttpRequestPtr &req,
               std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
-            auto expr = req->getParameter("expr");
+            try {
+              auto expr = req->getParameter("expr");
 
-            auto result = calc.evaluate(expr);
+              if (expr.empty()) {
+                Json::Value json;
+                json["error"] = "Missing expression";
 
-            Json::Value json;
-            json["result"] = result;
+                auto response = drogon::HttpResponse::newHttpJsonResponse(json);
 
-            auto response = drogon::HttpResponse::newHttpJsonResponse(json);
+                response->setStatusCode(drogon::k400BadRequest);
 
-            callback(response);
+                callback(response);
+                return;
+              }
+
+              auto result = calc.evaluate(expr);
+
+              Json::Value json;
+              json["result"] = result;
+
+              auto response = drogon::HttpResponse::newHttpJsonResponse(json);
+
+              callback(response);
+
+            } catch (const std::exception &e) {
+
+              Json::Value json;
+              json["error"] = e.what();
+
+              auto response = drogon::HttpResponse::newHttpJsonResponse(json);
+
+              response->setStatusCode(drogon::k400BadRequest);
+
+              callback(response);
+            }
           })
       .addListener("127.0.0.1", 8080)
       .run();
